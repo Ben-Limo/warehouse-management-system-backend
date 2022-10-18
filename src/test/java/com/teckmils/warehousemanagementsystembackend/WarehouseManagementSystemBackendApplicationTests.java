59b6b618d604aa70b780f2103473a6d1f0aa14ad
Ben Limo <bensokiplimo@gmail.com>
2022-10-18 10:37:51 +0300

    initial commit

---
+++ src/test/java/com/teckmils/warehousemanagementsystembackend/WarehouseManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teckmils.warehousemanagementsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
