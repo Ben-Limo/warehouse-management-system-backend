18d85eaa6f03b7c2c3a567cf1f5f33f6e1960daa
Ben Limo <bensokiplimo@gmail.com>
2022-11-30 12:52:33 +0300

    fix jasypt encryption exception

--- src/test/java/com/teckmils/warehousemanagementsystembackend/WarehouseManagementSystemBackendApplicationTests.java
+++ src/test/java/com/teckmils/warehousemanagementsystembackend/WarehouseManagementSystemBackendApplicationTests.java
@@ -2,8 +2,9 @@ package com.teckmils.warehousemanagementsystembackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(classes = WarehouseManagementSystemBackendApplicationTests.class)
 class WarehouseManagementSystemBackendApplicationTests {
 
 	@Test
